aacda77c57007e45404fee32d893ed768570e61c
Gianmaria Fadda <faddagianmaria10@gmail.com>
2024-09-10 22:18:34 +0200

    Quasi completata prima parte footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -9,9 +9,58 @@ export default {
 </script>
 
 <template>
-
+  <footer>
+    <div class="container">
+      <div class="row list">
+        <div class="col">
+        <h3>
+          DC COMICS
+        </h3>
+        <ul>
+          <li>
+            
+          </li>
+        </ul>
+        <h3>
+          SHOP
+        </h3>
+      </div>
+      <div class="col">
+        <h3>
+          DC
+        </h3>
+      </div>
+      <div class="col">
+        <h3>
+          SITES
+        </h3>
+      </div>
+      </div>
+      <div class="row">
+        <img src="../assets/img/dc-logo-bg.png" alt="dc-logo-bg">
+      </div>
+    </div>
+    <div class="container">
+      
+    </div>
+  </footer>
 </template>
 
 <style scoped>
-
+footer {
+  .container:first-child {
+    background-image: url(../assets/img/footer-bg.jpg);
+    display: flex;
+    justify-content: space-around;
+    .list {
+      display: flex;
+    }
+    .row {
+      img {
+        width: 350px;
+        object-fit: contain;
+      }
+    }
+  }
+}
 </style>
