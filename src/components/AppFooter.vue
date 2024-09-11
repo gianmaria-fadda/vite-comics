9b6b33f560858841154630a57008ef90dd983260
Gianmaria Fadda <faddagianmaria10@gmail.com>
2024-09-11 17:30:30 +0200

    Completato Footer pt. 1

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -2,7 +2,39 @@
 export default {
   data() {
     return{
-      
+      linkFirst: [
+        'Character',
+        'Comics',
+        'Movies',
+        'TV',
+        'Games',
+        'Videos',
+        'News'
+      ],
+      linkSecond: [
+        'Shop DC',
+        'Shop DC Collectibles'
+      ],
+      linkThird: [
+        'Term of Use',
+        'Privacy Policy (New)',
+        'Ad Choise',
+        'Adversting',
+        'Jobs',
+        'Subscription',
+        'Talent Workshop',
+        'CPSC Certificates',
+        'Ratings',
+        'Shop Help',
+        'Contact Us'
+      ],
+      linkFourth: [
+        'DC',
+        'MAD Magazine',
+        'DC Kids',
+        'DC Univerce',
+        'DC Power Visa'
+      ]
     }
   }
 }
@@ -13,28 +45,43 @@ export default {
     <div class="container">
       <div class="row list">
         <div class="col">
-        <h3>
-          DC COMICS
-        </h3>
-        <ul>
-          <li>
-            
-          </li>
-        </ul>
-        <h3>
-          SHOP
-        </h3>
-      </div>
-      <div class="col">
-        <h3>
-          DC
-        </h3>
-      </div>
-      <div class="col">
-        <h3>
-          SITES
-        </h3>
-      </div>
+          <h3>
+            DC COMICS
+          </h3>
+          <ul>
+            <li v-for="(link, i) in linkFirst">
+              {{ link }}
+            </li>
+          </ul>
+          <h3>
+            SHOP
+          </h3>
+          <ul>
+            <li v-for="(link, i) in linkSecond">
+              {{ link }}
+            </li>
+          </ul>
+        </div>
+        <div class="col">
+          <h3>
+            DC
+          </h3>
+          <ul>
+            <li v-for="(link, i) in linkThird">
+              {{ link }}
+            </li>
+          </ul>
+        </div>
+        <div class="col">
+          <h3>
+            SITES
+          </h3>
+          <ul>
+            <li v-for="(link, i) in linkFourth">
+              {{ link }}
+            </li>
+          </ul>
+        </div>
       </div>
       <div class="row">
         <img src="../assets/img/dc-logo-bg.png" alt="dc-logo-bg">
@@ -54,6 +101,22 @@ footer {
     justify-content: space-around;
     .list {
       display: flex;
+      .col {
+        padding-top: 40px;
+        h3 {
+          color: white;
+          padding: 15px 20px;
+        }
+        ul {
+          padding-left: 0;
+          li {
+            list-style: none;
+            padding: 1px 20px;
+            color: #818181;
+            font-size: .8rem;
+          }
+        }
+      }
     }
     .row {
       img {
